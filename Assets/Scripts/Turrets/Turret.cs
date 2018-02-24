9fcc343886bc994c4d82880a69b71f25b91db8ae
BuildTools <unconfigured@null.spigotmc.org>
2018-02-24 14:52:53 +0100

    Turret Rotation

---
+++ Assets/Scripts/Turrets/Turret.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    private Transform _target;
+
+    [SerializeField] private float _range = 15f;
+    [SerializeField] private string _enemyTag = "Enemy";
+    [SerializeField] private Transform _partToRotate;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(_enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= _range)
+        {
+            _target = nearestEnemy.transform;
+        }
+        else
+        {
+            _target = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (_target == null) return;
+
+        Vector3 dir = _target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = lookRotation.eulerAngles;
+        _partToRotate.rotation = Quaternion.Euler (-90f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _range);
+    }
+}
